cb0cf5d42849906bde96f37f23399af867291d60
maksie <dimedmax95@gmail.com>
2024-03-07 15:59:23 +0400

    first commit

---
+++ t_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+char[,] array = {
+    {'H', 'e', 'l', 'l', 'o'},
+    {'W', 'o', 'r', 'l', 'd'}
+};
+
+string result = "";
+
+for (int i = 0; i < array.GetLength(0); i++) {
+    for (int j = 0; j < array.GetLength(1); j++) {
+        result = result + array[i, j];
+    }
+}
+
+Console.WriteLine(result);
---
+++ t_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string originalString = "Hello";
+bool isPalindrom = true;
+
+for (int i = 0; i < originalString.Length / 2; i++)
+{
+    if (originalString[i] != originalString[originalString.Length - 1 - i])
+    {
+        isPalindrom = false;
+        break;
+    }
+}
+
+if (isPalindrom)
+{
+    Console.WriteLine("Строка является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Строка не является палиндромом.");
+}
